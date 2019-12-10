35808fb612a92388f4d6ea2e172a719364f38e2b
hewaele <hewaele@qq.com>
2019-12-10 10:04:28 +0800

    version 1.2 完善评测方案版本

--- c4_mask_nn/src/model_predict.py
+++ c4_mask_nn/src/model_predict.py
@@ -42,15 +42,18 @@ def img2pre(img, channel = 3):
 
 def plt_show(pre, mask, source):
     plt.figure(1)
-    plt.subplot(131)
-    plt.imshow(pre)
-    plt.subplot(132)
-    plt.imshow(mask)
-    plt.subplot(133)
-    plt.imshow(source)
+    if pre is not None:
+        plt.subplot(131)
+        plt.imshow(pre)
+    if mask is not None:
+        plt.subplot(132)
+        plt.imshow(mask)
+    if source is not None:
+        plt.subplot(133)
+        plt.imshow(source)
     plt.show()
 
-def eval_A(pre, mask, image=None):
+def eval_B(pre, mask, image=None):
     """
     :param pre: Image type  [256, 256, 1]
     :param mask: Image type [256, 256, 4]
@@ -79,42 +82,78 @@ def eval_A(pre, mask, image=None):
     precision = TP/(TP+FP+0.0000001)
     recall = TP/(TP+FN+0.000001)
     F1 = (2*precision*recall)/(precision+recall+0.00001)
-    return ac, precision, recall, F1
+    return ac, precision, recall, F1, TP, FP, TN, FN
 
-model_path = '../log/my_model_20191206-185825_source_v1.h5'
-model = creat_my_model()
-model.load_weights(model_path)
+def start_eval(star, end, step=1, show=False):
+    os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+    model_path = '../log/source_v1/my_model.h5'
+    model = creat_my_model()
+    model.load_weights(model_path)
 
-#载入测试数据
-img_path = '../data/CoMoFoD_small/'
-x_list, y_list = filter_image(img_path)
+    #载入测试数据
+    img_path = '../data/CoMoFoD_small/'
+    x_list, y_list = filter_image(img_path)
 
-star = 4000
-end = 5000
-step = 1
-ac = 0
-precision = 0
-recall = 0
-F1 = 0
-count = 0
-for x, y in zip(x_list[star:end:step], y_list[star:end:step]):
-    img_data = img2pre(os.path.join(img_path, x))
-    pre = model.predict(img_data/255)
+    ac = 0
+    precision = 0
+    recall = 0
+    F1 = 0
+    count = 0
+    TP, FP, TN, FN = 0, 0, 0, 0
+    for x, y in zip(x_list[star:end:step], y_list[star:end:step]):
+        print(x)
+        img_data = img2pre(os.path.join(img_path, x))
+        pre = model.predict(img_data/255)
+
+        show_pre = pre2img(pre)
+        show_y = Image.open(os.path.join(img_path, y)).resize([256, 256])
+        show_x = Image.open(os.path.join(img_path, x)).resize([256, 256])
+
+        # Image.Image.show(show_pre)
+        # Image.Image.show(show_y)
+        # Image.Image.show(show_x)
+        if show:
+            plt_show(show_pre, show_y, show_x)
+        a, b, c, d, tp, fp, tn, fn= eval_B(show_pre, show_y)
+
+        TP += tp
+        FP += fp
+        TN += tn
+        FN += fn
+
+        ac += a
+        precision += b
+        recall += c
+        F1 += d
+        count += 1
+
+    print('protocal A: ac:{} precision:{} recall:{} F1:{}'.
+          format((TP+TN)/(TP+TN+FP+FN), TP/(TP+FP), TP/(TP+FN), 2*TP/(2*TP+FP+FN)))
+
+    print('protocal B: ac:{} precision:{} recall:{} F1:{}'.
+          format(ac/count, precision/count, recall/count, F1/count))
+
+def pre_test_img(img_path):
+    os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+    model_path = '../log/source_v1/my_model.h5'
+    model = creat_my_model()
+    model.load_weights(model_path)
+
+    img_data = img2pre(img_path)
+    pre = model.predict(img_data / 255)
 
     show_pre = pre2img(pre)
-    show_y = Image.open(os.path.join(img_path, y)).resize([256, 256])
-    show_x = Image.open(os.path.join(img_path, x)).resize([256, 256])
+    show_x = Image.open(img_path).resize([256, 256])
 
-    # Image.Image.show(show_pre)
-    # Image.Image.show(show_y)
-    # Image.Image.show(show_x)
+    Image.Image.show(show_pre)
+    Image.Image.show(show_x)
 
-    # plt_show(show_pre, show_y, show_x)
-    a, b, c, d = eval_A(show_pre, show_y)
-    ac += a
-    precision += b
-    recall += c
-    F1 += d
+    plt_show(show_pre, None, show_x)
 
-print('ac:{} precision:{} recall:{} F1:{}'.
-      format(ac/1000*25, precision/1000*25, recall/1000*25, F1/1000*25))
+def main():
+    start_eval(4000, 5000, show=False)
+    # pre_test_img('../test_img/161_F.png')
+
+if __name__ == "__main__":
+    main()
+
